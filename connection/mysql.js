3328ea2f6e8a29e8df076324d858b535f74d72b4
mubarkkhan <ahad771418@gmail.com>
2025-05-25 00:25:06 +0530

    Initial commit

--- connection/mysql.js
+++ connection/mysql.js
@@ -1,23 +1,26 @@
-const mysql = require('mysql')
+const mysql = require("mysql");
+require("dotenv").config();
 
 async function connectMySQL() {
-    const connection = mysql.createConnection({
-        host: "127.0.0.1",
-        user: "root",
-        password: "",
-        database : "db_portfolio"
-    })
-    return new Promise((resolve, reject) => {
-        connection.connect((err) => {
-            if (err) {
-                reject(err)
-                return;
-            }
-            else{resolve}
-        })
-    })
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+  return new Promise((resolve, reject) => {
+    connection.connect((err) => {
+      if (err) {
+        reject(err);
+        return;
+      } else {
+        resolve;
+      }
+    });
+  });
 }
 
 module.exports = {
-    connectMySQL
-}
+  connectMySQL,
+};
--- connection/pool.js
+++ connection/pool.js
@@ -2,10 +2,11 @@ const mysql = require("mysql");
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  server: "127.0.0.1",
-  user: "root",
-  password: "",
-  database: "db_portfolio",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 module.exports = pool;
